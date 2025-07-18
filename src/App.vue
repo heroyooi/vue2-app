6bfd3af46c4285305c302b197590961f47fb765e
heroyooi <heroyooi1018@gmail.com>
2025-07-18 16:06:01 +0900

    1강: Vue 2 개발 환경 세팅

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Vue 2.6.14 앱이 성공적으로 실행되었습니다!',
+    };
+  },
+};
+</script>
